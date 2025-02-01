8b2d656c9dc6930b7e5463cec6cd6c4c2e53ac63
nitin <nitingupta10a@gmail.com>
2025-02-01 11:50:44 +0530

    Added dynamic resource checking and machine on/off functionality.

--- Coffee_Vending_machine/main.py
+++ Coffee_Vending_machine/main.py
@@ -1,23 +1,28 @@
 def check_resources(rec):
-    for key,value in resources.items():
+    for key, value in resources.items():
         print(f"{key} : {value}")
+
 def espresso(tot):
-    if resources['water']>MENU['espresso']['ingredients']['water']:
+    if resources['water'] > MENU['espresso']['ingredients']['water']:
         resources['water'] -= MENU['espresso']['ingredients']['water']
         if resources['coffee'] > MENU['espresso']['ingredients']['coffee']:
             resources['coffee'] -= MENU['espresso']['ingredients']['coffee']
         else:
             print("Sorry there is not enough coffee !!")
+            return
     else:
         print("Sorry there is not enough water !!")
+        return
+
     print(tot)
     if tot < MENU['espresso']['cost']:
         print("Sorry that's not enough money. Money refunded.")
-    elif tot > MENU['espresso']['cost']:
-        change = round(tot - int(MENU['espresso']['cost']), 3)
-        print(f'Here is ${change}.0 in change.')
+    else:
+        change = round(tot - MENU['espresso']['cost'], 2)
+        print(f'Here is ${change} in change.') if change > 0 else None
         print("Here is your espresso ☕️. Enjoy!")
         resources['money'] += MENU['espresso']['cost']
+
 def latte(tot):
     if resources['water'] > MENU['latte']['ingredients']['water']:
         resources['water'] -= MENU['latte']['ingredients']['water']
@@ -27,18 +32,23 @@ def latte(tot):
                 resources['milk'] -= MENU['latte']['ingredients']['milk']
             else:
                 print("Sorry there is not enough milk !!")
+                return
         else:
             print("Sorry there is not enough coffee !!")
+            return
     else:
         print("Sorry there is not enough water !!")
+        return
+
     print(tot)
     if tot < MENU['latte']['cost']:
         print("Sorry that's not enough money. Money refunded.")
-    elif tot > MENU['latte']['cost']:
-        change = round((tot - float(MENU['latte']['cost'])))
-        print(f'Here is ${change}.0 in change.')
+    else:
+        change = round(tot - MENU['latte']['cost'], 2)
+        print(f'Here is ${change} in change.') if change > 0 else None
         print("Here is your latte ☕️. Enjoy!")
         resources['money'] += MENU['latte']['cost']
+
 def cappuccino(tot):
     if resources['water'] > MENU['cappuccino']['ingredients']['water']:
         resources['water'] -= MENU['cappuccino']['ingredients']['water']
@@ -48,25 +58,31 @@ def cappuccino(tot):
                 resources['milk'] -= MENU['cappuccino']['ingredients']['milk']
             else:
                 print("Sorry there is not enough milk !!")
+                return
         else:
             print("Sorry there is not enough coffee !!")
+            return
     else:
         print("Sorry there is not enough water !!")
+        return
+
     print(tot)
     if tot < MENU['cappuccino']['cost']:
         print("Sorry that's not enough money. Money refunded.")
-    elif tot > MENU['cappuccino']['cost']:
-        change = (tot - float(MENU['cappuccino']['cost']))
-        print(f'Here is ${change}.0 in change.')
+    else:
+        change = round(tot - MENU['cappuccino']['cost'], 2)
+        print(f'Here is ${change} in change.') if change > 0 else None
         print("Here is your cappuccino ☕️. Enjoy!")
         resources['money'] += MENU['cappuccino']['cost']
+
 def input_coins():
     print("Please insert coins.\n")
-    quaters = int(input("how many quarters?:"))
-    dimes = int(input("how many dimes?:"))
-    nickles = int(input("how many nickles?:"))
-    pennies = int(input("how many pennies?:"))
-    return float((quaters * 0.25) + (dimes * 0.1) + (nickles * 0.05) + (pennies * 0.01))
+    quaters = int(input("how many quarters?:")) * 0.25
+    dimes = int(input("how many dimes?:")) * 0.10
+    nickles = int(input("how many nickles?:")) * 0.05
+    pennies = int(input("how many pennies?:")) * 0.01
+    return round(quaters + dimes + nickles + pennies, 2)
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -99,18 +115,17 @@ resources = {
     "coffee": 100,
     "money": 0
 }
+
 while True:
     coffee = input("What would you like? (espresso/latte/cappuccino):").lower()
     if coffee == 'report':
         check_resources(resources)
-    if coffee == 'espresso':
+    elif coffee == 'espresso':
         espresso(input_coins())
     elif coffee == 'latte':
         latte(input_coins())
     elif coffee == 'cappuccino':
         cappuccino(input_coins())
     elif coffee == 'off':
-        print("Coffee Maachine Turned Of Successfully")
+        print("Coffee Machine Turned Off Successfully.")
         break
-
-
